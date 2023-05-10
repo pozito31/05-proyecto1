4367d4c50f6380e1b97e643544b3a996ed4c78ba
pozito31 <jessicapozo198715@gmail.com>
2023-05-10 17:50:09 +0200

    actualizando cambios

--- src/components/Crear.js
+++ src/components/Crear.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Crear = () => {
-    return (
-        <div className="add">
-          <h3 className="title">Añadir pelicula</h3>
-          <form>
-            <input type="text" placeholder="Titulo" />
-            <textarea placeholder="Descripcion"></textarea>
-            <input type="submit" value="Guardar" />
-          </form>
-        </div>
-    );
+
+  const titulo = "Añadir pelicula";
+  const [peliState, setPeliState] = useState({
+    titulo: '',
+    descripcion: ''
+  });
+
+  const conseguirDatorForm = e => {
+    e.preventDefault();
+
+    //Conseguir datos del formulario
+    let target = e.target;
+    let titulo = target.titulo.value;
+    let descripcion = target.descripcion.value;
+
+    //Crear objeto de La pelicula a guardar
+    let peli = {
+      id: new Date().getTime(),
+      titulo,
+      descripcion
+    };
+
+    setPeliState(peli);
+
+    console.log(peliState);
+  }
+
+  return (
+    <div className="add">
+      <h3 className="title">{titulo}</h3>
+
+      <form onSubmit={conseguirDatorForm}>
+        <input type="text"
+          id="titulo"
+          name="titulo"
+          placeholder="Titulo" />
+        
+        <textarea
+          id="descripcion"
+          name="descripcion"
+          placeholder="Descripcion"></textarea>
+        
+        <input type="submit"
+          id="save"
+          value="Guardar" />
+        
+      </form>
+    </div>
+  );
 }
